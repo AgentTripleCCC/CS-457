dfa93b2d0feca5273472ef04df0fec559e3ff866
AgentTripleCCC <agenttripleccc@gmail.com>
2020-02-06 16:06:33 -0800

    first commit

---
+++ database.h
@@ -0,0 +1,14 @@
+/*
+	Database Header file
+	CS 457, from University of Nevada, Reno
+	by Curtis Chung
+	Date: 2/6/2020
+*/
+#ifndef DATABASE_H
+#define DATABASE_H
+
+class Database {
+public:
+	Database();
+
+};
---
+++ main.cpp
@@ -0,0 +1,10 @@
+/*
+	Project Assignment 1, written in C++
+	CS 457, from University of Nevada, Reno
+	by Curtis Chung
+*/
+
+int main () 
+{
+	return 0;
+}
